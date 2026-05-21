5314c51b56af3d6ad7fd9c4760dc923c752768e3
Fred Charbonnier <frederickcharbonnier@icloud.com>
2026-05-22 01:46:52 +0200

    Drastically simplified the code for CLabs. Removed the LED functionality and removed manual control and state machines for easy of use.

--- src/main.cpp
+++ src/main.cpp
@@ -1,164 +1,70 @@
 #include <Arduino.h>
-#include <Servo.h>
-#include <LiquidCrystal_I2C.h>
-# define LCD_ACTIVE 0
-
-// Button Inputs:
-// Middle 500 500
-// X axis 0-1024 left to right
-// Y axis 0-1024 top to bottom
-// Button on 1 as standard and 0 when pressed
-
-enum ControlMode {
-	MANUAL,
-	SWEEP
-};
+#include <ESP32Servo.h>
 
 // Pins
-const int trigPin = 6;
-const int echoPin = 11;
-const int redLEDPin = 9;
-const int greenLEDPin = 8;
-const int xAxisPin = A2;
-const int yAxisPin = A3;
-const int joyStickPin = 13;
-const int buttonPin = 7;
+const int trigPin = 5;
+const int echoPin = 18;
+const int servoPin = 23;
 
-// Variables
-long duration;
-long cm;
-bool lastButtonState = HIGH;
-int angle = 90;
-ControlMode mode = MANUAL;
-// LiquidCrystal_I2C lcd(0x27, 16, 2); // Lcd address is 0x27
 Servo servo;
-// Sweep state variables
-int sweepAngle = 10;
-int sweepDirection = 1;
-unsigned long lastSweepTime = 0;
-const unsigned long sweepInterval = 15;
 
+int angle = 0;
+int direction = 1;
 
-// Functions
-void dangerLight(long distance);
-void sonarPulse(int angle);
-void changeMode(ControlMode *mode);
-void handleManual();
-void handleSweep();
-
-void setup() 
-{
-	// Display
-	// lcd.init();
-	// lcd.clear();
-	// lcd.backlight();
-
-	// Servo
-	servo.attach(10);
-	servo.write(90);
-
-	// Pins
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, INPUT);
-	pinMode(redLEDPin, OUTPUT);
-	pinMode(greenLEDPin, OUTPUT);
-	pinMode(joyStickPin, INPUT_PULLUP);
-	pinMode(buttonPin, INPUT_PULLUP);
-	// Serial Monitor
-	Serial.begin(9600);
-}
-
-void loop() 
-{
-	bool	buttonState = digitalRead(buttonPin);
-	
-	if (buttonState == LOW && lastButtonState == HIGH)
-		changeMode(&mode);
-	lastButtonState = buttonState;
-	if (mode == SWEEP)
-		handleSweep();
-	if (mode == MANUAL)
-		handleManual();
-}
-
-// Functions:
-
-void dangerLight(long distance)
-{
-	if( distance < 30)
-	{
-		digitalWrite(redLEDPin, HIGH);
-		digitalWrite(greenLEDPin, LOW);
-		// if (LCD_ACTIVE)
-		// {
-		// 	lcd.setCursor(2,1);
-		// 	lcd.print("DANGER HERE!    ");
-		// }
-	}
-	else
-	{
-		digitalWrite(redLEDPin, LOW);
-		digitalWrite(greenLEDPin, HIGH);
-		// if (LCD_ACTIVE)
-		// {
-		// 	lcd.setCursor(2, 1);
-		// 	lcd.print("You are safe    ");
-		// }
-	}
-}
-
-void sonarPulse(int angle)
+long getDistance()
 {
 	digitalWrite(trigPin, LOW);
-	delayMicroseconds(5);
+	delayMicroseconds(2);
+
 	digitalWrite(trigPin, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(trigPin, LOW);
-	duration = pulseIn(echoPin, HIGH, 25000);
+	// Reduced timeout for faster sweep
+	long duration = pulseIn(echoPin, HIGH, 3000);
+	// No echo received
 	if (duration == 0)
-		return ;
-	cm = duration * 0.034 / 2;
-	// if (LCD_ACTIVE)
-	// {
-	// 	lcd.setCursor(0,0);
-	// 	lcd.print("Distance: ");
-	// 	lcd.print(cm);
-	// 	lcd.print("cm   ");
-	// }
-	dangerLight(cm);
+		return 9999;
+	long distance = duration * 0.034 / 2;
+	// // Clamp max range to 30 cm
+	// if (distance > 30)
+	// 	distance = 300;
+	return distance;
 }
 
-void	changeMode(ControlMode *mode)
+void setup()
 {
-	if (*mode == SWEEP)
-		*mode = MANUAL;
-	else
-		*mode = SWEEP;
-}
+	Serial.begin(115200);
 
-void handleSweep()
-{
-	if (millis() - lastSweepTime < sweepInterval)
-		return ;
-	lastSweepTime = millis();
-	servo.write(sweepAngle);
-	sonarPulse(sweepAngle);
-	sweepAngle += sweepDirection;
-	if (sweepAngle <= 10 || sweepAngle >= 170)
-		sweepDirection = -sweepDirection;
-}
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
 
-void handleManual()
-{
-	int	xPosition = analogRead(xAxisPin);
-	int	joyStickState = digitalRead(joyStickPin);
+	servo.attach(servoPin);
 
-	if (joyStickState == 0)
-		angle = 90;
-	if (xPosition < 450 && angle < 170)
-		angle += 3;
-	else if (xPosition > 550 && angle > 10)
-		angle -= 3;
 	servo.write(angle);
-	sonarPulse(angle);
+
+	delay(500);
+}
+
+void loop()
+{
+	servo.write(angle);
+
+	// Small settle delay
+	delay(5);
+
+	long distance = getDistance();
+
+	Serial.print(angle);
+	Serial.print(",");
+	Serial.println(distance);
+
+	// Smoother sweep
+	angle += direction;
+
+	// Full sweep
+	if (angle >= 180 || angle <= 0)
+		direction *= -1;
+
+	// Faster sweep speed
+	delay(8);
 }
